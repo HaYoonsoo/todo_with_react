f80bd0edc81bd15bf91ff510acd24d63dda3640b
HaYoonsoo <ahakilo103@gmail.com>
2022-05-30 17:49:47 +0900

    todo redux 생성후 provider까지 연결

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( (
-  <App />
-));
+import App from "./App";
+import todoReducer from "./modules/todoReducer";
+
+const store = createStore(todoReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
