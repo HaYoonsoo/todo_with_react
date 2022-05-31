893fdf97ceaca20527cbb71b91da0948e377b4ec
HaYoonsoo <ahakilo103@gmail.com>
2022-05-31 14:31:08 +0900

    useMemo로 변경
    useMemo가 더 효율적인 구조라고 한다

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,17 @@
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  const [todoNumbers, setTodoNumbers] = useState({ total: 0, done: 0 });
   const todoList = useSelector((state) => state.todoList);
 
-  useEffect(() => {
-    let total = 0,
-      done = 0;
-    total = todoList.length;
-    todoList.map((todo) => (todo.done ? done++ : false));
-
-    setTodoNumbers({ total, done });
-  }, [todoList]);
+  const total = useMemo( () => todoList.length, [todoList]);
+  const done = useMemo( () => todoList.filter( todo => todo.done ).length, [todoList]);
 
   return (
     <>
       <h1>Header</h1>
       <span>
-        {todoNumbers.total - todoNumbers.done} / {todoNumbers.total}
+        {total - done} / {total}
       </span>
     </>
   );
