3ba28a679ac4df7699c965806c2541ab86b5c187
HaYoonsoo <ahakilo103@gmail.com>
2022-05-31 11:34:38 +0900

    components 생성
    App 기능 삭제하고 components로 만드는중

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-/*
-import React from 'react';
+import React, { useState } from "react";
+import styled from 'styled-components';
 import './App.css';
 
-const App = () => {
-  return (
-    <h1>Hi</h1>
-  );
-}
+import Header from './components/Header';
+import List from './components/List';
+import InputBox from './components/InputBox';
 
-export default App;
-*/
+const AppBody = styled.div`
 
-//기능 구현 후에 컴포넌트를 나누자
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addTodo, toggleTodo } from "./modules/todo";
+`;
 
 const App = () => {
-  const [text, setText] = useState('');
-  const todoList = useSelector((state) => state.todoList);
-  const dispatch = useDispatch();
-
-  const add = () => {
-    dispatch(addTodo(text));
-    setText('');
-  };
-  const toggle = (id) => {
-    dispatch(toggleTodo(id))
-  };
-  const onChange = (e) => {
-    setText(e.target.value);
-  }
-
   return (
     <>
-      {todoList.map((todo) => (
-        <h1 key={todo.id} onClick={ () => toggle(todo.id) }>{todo.id} / {todo.text} / {todo.done ? 'true' : 'false'}</h1>
-      ))}
-      <input value={text} onChange={onChange}/>
-      <button onClick={add}>추가</button>
+      <AppBody> {/*
+        <Header />
+        <List />
+  <InputBox /> */}
+      </AppBody>
     </>
   );
 };
---
+++ src/components/InputBox/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InputBox = () => {
+  return (
+    <h1>InputBox</h1>
+  );
+}
+
+export default InputBox;
